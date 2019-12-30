f7231e9646f00bcc7d0cba76288356b65c1fd597
Matt Bromley <bwileymwb@gmail.com>
2019-12-29 20:40:25 -0500

    add some logging

--- __init__.py
+++ __init__.py
@@ -8,6 +8,8 @@ class ShoppingList(MycroftSkill):
 	@intent_file_handler('add.to.shopping.list.intent')
 	def handle_add_to_list(self, message):
 		item1_name = message.data.get('item1')
+		self.log.info(message)
+		self.log.info('item1 is: ' + item1_name)
 		item2_name = message.data.get('item2')
 
 		self.speak_dialog('add.to.shopping.list', {'item1': item1_name})
