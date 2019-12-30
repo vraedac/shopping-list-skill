f3275b8e3b643e3c6308d7c5e86aa19ea1b8b0b0
Matt Bromley <bwileymwb@gmail.com>
2019-12-29 19:38:15 -0500

    Fix tabs in __init__.py

--- __init__.py
+++ __init__.py
@@ -2,15 +2,15 @@ from mycroft import MycroftSkill, intent_file_handler
 
 
 class ShoppingList(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
+	def __init__(self):
+		MycroftSkill.__init__(self)
 
-    @intent_file_handler('list.shopping.intent')
-    def handle_list_shopping(self, message):
+	@intent_file_handler('list.shopping.intent')
+	def handle_list_shopping(self, message):
 		item_name = message.data.get('item')
-        self.speak_dialog('list.shopping', {'item': item_name})
+		self.speak_dialog('list.shopping', {'item': item_name})
 
 
 def create_skill():
-    return ShoppingList()
+	return ShoppingList()
 
