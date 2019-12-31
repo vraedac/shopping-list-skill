1091f6c5ae2f5f2555dc661935493e382713c60d
Matt Bromley <bwileymwb@gmail.com>
2019-12-30 21:13:47 -0500

    First attempt at getting the "add item to list" intent to actually add something on Todoist

--- __init__.py
+++ __init__.py
@@ -9,10 +9,21 @@ class ShoppingList(MycroftSkill):
 
 	@intent_file_handler('list.shopping.intent')
 	def handle_list_shopping(self, message):
-		self.todoist_api.sync()
-		self.log.info('about to print projects state:')
-		self.log.info(self.todoist_api.state['projects'])
 		item_name = message.data.get('item')
+		self.todoist_api.sync()
+		projects = self.todoist_api.state['projects']
+		self.log.info('about to print projects state:')
+		self.log.info(projects)
+
+		listProject = None
+		for proj in projects:
+			if proj.name == 'Grocery List':
+				listProject = proj
+			
+		if listProject is not None:
+			todoist_api.items.add(item_name, project_id=listProj['id'])
+			api.commit()
+
 		self.speak_dialog('list.shopping', {'item': item_name})
 	
 	@intent_file_handler('remove.from.shopping.list.intent')
